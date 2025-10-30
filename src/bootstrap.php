4ee670aef360ad5852b74c86618ee52df2d8a6c0
thespacebean <rachelbeansloan@gmail.com>
2025-10-30 14:02:04 +0000

    Implement twig example on index page

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,8 +1,23 @@
 <?php
 
-$autoload = __DIR__ . '/../vendor/autoload.php';
-if (file_exists($autoload)) {
-    require_once $autoload;
+$autoloadCandidates = [
+    __DIR__ . '/vendor/autoload.php',
+    __DIR__ . '/../vendor/autoload.php',
+];
+foreach ($autoloadCandidates as $autoload) {
+    if (file_exists($autoload)) {
+        require_once $autoload;
+        break;
+    }
 }
 
+require_once __DIR__ . '/app/bootstrap_runtime.php';
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+$loader = new FilesystemLoader(__DIR__ . '/app/templates');
+$twig = new Environment($loader);
+
+
+return ['twig' => $twig];
--- src/index.php
+++ src/index.php
@@ -1,16 +1,14 @@
 <?php
 
 use App\Examples\SquareShape;
-
-require __DIR__ . '/bootstrap.php';
+$app = require __DIR__ . '/bootstrap.php';
+$twig = $app['twig'];
 
 $shape = new SquareShape('blue');
 
 $text = $shape->showDetails();
 
-echo $text;
+echo $twig->render('example.html.twig', ['text' => $text]);
 
 echo "<p>This should show the database test:</p>";
-echo "<br /><br />";
 include __DIR__ . '/database_test.php';
-echo "<br>";
